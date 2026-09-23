60727f3d614a031504cf8995a2faa4d1b2b4e276
kingyx3 <kingyx3@hotmail.com>
2026-09-23 15:03:44 +0800

    Compile Android Kotlin sources in APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,12 @@
-plugins { id("com.android.application") }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
 android {
     namespace = "io.github.kingyx3.autoclicker"
     compileSdk = 35
     defaultConfig { applicationId = "io.github.kingyx3.autoclicker"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "0.1.0" }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
+    kotlinOptions { jvmTarget = "17" }
 }
